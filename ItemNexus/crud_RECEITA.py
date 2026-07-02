a806307aa3c0c076abc606168b134c09448adef7
Maryyyy17 <liberatomari35@gmail.com>
2026-07-02 21:06:00 +0000

    receita finalizada

--- ItemNexus/crud_RECEITA.py
+++ ItemNexus/crud_RECEITA.py
@@ -27,3 +27,77 @@ async def criar_itemc(rct: Receita ) :
         cur.close()
         conn.close()
     return {"msg:" "Receita criado com sucesso"}
+
+
+@router.get("/receita", response_model = List[Receita])
+async def listar_receita():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, nome, id_resultado FROM Receita ")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return [
+        Receita(
+            id = i[0], nome = i[1], id_resultado = i[2]
+        ) for i in rows
+    ]
+
+
+@router.get("/receita/{id}", response_model = Receita)
+async def get_receita(id : int):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, nome, id_resultado FROM Receita WHERE id = %s", (id,))
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    if row:
+        return Receita( id = i[0], nome = i[1], id_resultado = i[2])
+    raise HTTPException (404, "Receita não encontrada")
+
+
+@router.patch("/receita/{id}")
+async def att_receita_parcial(id: int, rct: UpdateReceita):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM Receita WHERE id = %s", (id,))
+    if not cur.fetchone():
+        cur.close()
+        conn.close()
+        raise HTTPException(404, "Departamento não encontrado")
+    fields = []
+    values = []
+    for campo, valor in rct.dict(exclude_unset = True).items():
+        fields.append(f"{campo} = %s")
+        values.append(valor)
+
+    if not fields:
+        cur.close()
+        conn.close()
+        raise HTTPException (400, "Nenhum campo informado para atualização")
+    values.append(id)
+    try:
+        cur.execute("UPDATE Receita SET {', '.join(fields)} WHERE dnumero=%s", values)
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(400, f"Erro ao atualizar: {e}")
+    
+    finally:
+        cur.close()
+        conn.close()
+
+    return {"msg": "Receita Atualizada"}
+
+
+@router.delete("/receita/{id}")
+async def deletar_receita(id:int):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM Receita WHERE id=%s", (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"msg": "Receita removida"}
