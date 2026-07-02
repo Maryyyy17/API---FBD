ed446c2b5b3dbea305eea2c5ea35acda90fa02e5
Maryyyy17 <liberatomari35@gmail.com>
2026-07-02 20:31:37 +0000

    crud dono finalizado

--- ItemNexus/crud_DONO.py
+++ ItemNexus/crud_DONO.py
@@ -29,33 +29,25 @@ async def criar_dono(dn: Dono) :
     return {"msg:" "Dono criado com sucesso"}
 
 
-@router.get("/dono")
+
+@router.get("/dono", response_model = List[Dono])
 async def listar_dono():
     conn = get_connection()
     cur = conn.cursor()
-
-    cur.execute(
-        "SELECT id_dono, tipo, ativo, data_inativacao FROM Dono "
-    )
-
+    cur.execute("SELECT id_dono, tipo, ativo, data_inativacao FROM Dono")
     rows = cur.fetchall()
-
     cur.close()
     conn.close()
-
     return [
-        Dono(
-            id_dono = i[0],
-            tipo = i[1],
-            ativo = i[2],
-            data_inativacao = i[3]
-        ) for i in rows
+        Dono(id_dono = i[0], tipo = i[1], ativo = i[2], data_inativacao = i[3]
 
-    ] 
+        ) for i in rows
+    ]
+
 
 
 @router.get("/dono/{id_dono}")
-async def listar_dono_by_id(id_dono: int) :
+async def get_dono(id_dono: int) :
     conn = get_connection()
     cur = conn.cursor()
 
@@ -74,3 +66,59 @@ async def listar_dono_by_id(id_dono: int) :
 
 
 
+@router.get("/dono")
+async def att_dono(id_dono: int, dn: Update_Dono):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT id_dono FROM Dono WHERE id_dono = %s, (id_dono) "
+    )
+
+    if not cur.fetchone():
+        cur.close()
+        conn.close()
+        raise HTTPException(404, f"Dono não encontrado")
+    
+    fields = []
+    values = []
+
+    for campo, valor in dn.dict(exclude_unset= True).items():
+        fields.append(f"{campo}=%s")
+        values.append(valor)
+
+
+    if not fields:
+        cur.close()
+        conn.close()
+        raise HTTPException(400, "Nenhum campo informado na atualização")
+    
+    try:
+        cur.execute(
+            f"UPDATE departamento SET {','.join(fields)} WHERE id_dono=%s", values
+        )
+        conn.commit()
+
+
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException (400, f"Erro ao atualizar departamento {e}")
+    
+
+    finally:
+        cur.close()
+        conn.close()
+
+    return {"msg": "Departamento atualizado"}
+
+
+
+@router.delete("/dono/{id_dono}")
+async def deletar_dono(id_dono: int):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM DONO WHERE id_dono = %s", (id_dono))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"msg": "Dono removido"}
