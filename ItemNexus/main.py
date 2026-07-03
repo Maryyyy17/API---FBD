42b4e2730d857b2ad4ab39a69ba25a5372e893a8
Maryyyy17 <liberatomari35@gmail.com>
2026-07-03 02:00:14 +0000

    att

--- ItemNexus/main.py
+++ ItemNexus/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from crud_DONO import router as dono_router
 from crud_RECEITA import router as receita_router
+from crud_GUILDA import router as guilda_router
+from crud_JOGADOR import router as jogador_router
 
 app = FastAPI(
     title = "API ITEM NEXUS",
@@ -9,3 +11,5 @@ app = FastAPI(
 
 app.include_router(dono_router, prefix="/dono")
 app.include_router(receita_router, prefix="/receita")
+app.include_router(guilda_router, prefix="/guilda")
+app.include_router(jogador_router, prefix="/jogador")
